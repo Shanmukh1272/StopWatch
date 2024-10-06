d11a5657155cf21ce0cc430d549ad827dd09667a
sha <111038850+Shanmukh1272@users.noreply.github.com>
2024-10-06 21:45:04 +0530

    Add files via upload
    I have created this application using ReactJs

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import StopWatch from './StopWatch.jsx';
+import React from 'react';
+
+function App() {
+
+   return(
+      <>
+      <StopWatch/>
+      <StopWatch/>
+      <StopWatch/>
+      </>);
+    }
+
+
+export default App
